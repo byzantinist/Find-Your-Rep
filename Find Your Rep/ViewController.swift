bc1ec803e7f7d28b3d6046202b49b19e035f4db0
Eric Chiang <echiang777@yahoo.com>
2017-07-05 09:54:54 -0500

    Initial Commit

---
+++ Find Your Rep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Your Rep
+//
+//  Created by Eric Chiang on 7/5/17.
+//  Copyright © 2017 Eric Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
