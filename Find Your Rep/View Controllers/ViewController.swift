6ba88551effbaa24415fe2d7cf4228132f36877f
Eric Chiang <echiang777@yahoo.com>
2017-07-05 11:29:17 -0500

    Implement Segue Buttons
    Figured out how to use segues correctly.

--- Find Your Rep/View Controllers/ViewController.swift
+++ Find Your Rep/View Controllers/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+// API Key:
+// mxppcLKQTS3Cu2eMKrZsr2Kp3L795AIs2fc1jtCR
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zipCodeField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var filterSelector: UITextField!
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
